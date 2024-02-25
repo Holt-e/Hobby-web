7d8f12e875afa8c558302114b78a3956fee0fb90
Adrian Holthe <adrian.holthe@aibel.com>
2024-02-25 20:01:54 +0100

    Added Template

---
+++ src/assets/CV/cv.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CvContainer = styled.main`
+    background-color: aliceblue;
+    `;
+
+CvContainer.defaultProps = {};
---
+++ src/assets/CV/cv.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { CvContainer } from "./cv.styled";
+
+export const Home = () => {
+    return <CvContainer>TODO</CvContainer>;
+};
